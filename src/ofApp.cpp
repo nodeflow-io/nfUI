749c8641c76de3d9cc5db223217c8f1c978b262d
Matthias Strohmaier <ms@hostbuster.at>
2024-02-14 20:37:39 +0100

    added entity-component system

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,10 +1,14 @@
 #include "ofApp.h"
 
-ofTrueTypeFont font;
+
 
 //--------------------------------------------------------------
 void ofApp::setup() {
     ofSetLogLevel(OF_LOG_VERBOSE);
+    ofDisableAntiAliasing();
+    ofTrueTypeFont font;
+    font.load("/System/Library/Fonts/Supplemental/Times New Roman.ttf", 18);
+    /*
     monoLineTextInput.setup();
     monoLineTextInput.text = "hello world";
     monoLineTextInput.bounds.x = 100;
@@ -36,8 +40,32 @@ void ofApp::setup() {
     multilineTextInput.bounds.width = 400;
     multilineTextInput.bounds.height = 500;
     multilineTextInput.multiline = true;
-    font.loadFont(OF_TTF_SERIF, 18);
-    monoLineTextInput.setFont(font);
+    fontdemo.loadFont(OF_TTF_SERIF, 18);
+    monoLineTextInput.setFont(fontdemo);
+     
+    
+    myParameter.set("My Parameter", "testingclass");
+    
+    // Initialize ParameterDisplay with x, y, width, height
+    parameterDisplay = new ParameterDisplay(myParameter, 100, 400, 188, 40);
+     */
+    // Add NFValues to the node with labels
+    ofxTextInputField textInputField1;
+    ofxTextInputField textInputField2;
+    ofxTextInputField textInputField3;
+    ofxTextInputField textInputField4;
+    
+    textInputFields.push_back(textInputField1);
+    textInputFields.push_back(textInputField2);
+    textInputFields.push_back(textInputField3);
+    textInputFields.push_back(textInputField4);
+    
+    // Add NFValues to the node with labels and set up textInputFields
+    nfNode.addNFValue<StringNFValue, std::string>("String Value", "Hello", textInputFields[0], 200, 220, 100, 20, font);
+    nfNode.addNFValue<DoubleNFValue, double>("Double Value", 3.14, textInputFields[1], 200, 240, 100, 20, font);
+    nfNode.addNFValue<BoolNFValue, bool>("Bool Value", true, textInputFields[2], 200, 260, 100, 20, font);
+    nfNode.addNFValue<IntNFValue, int>("Int Value", 42, textInputFields[3], 200, 280, 100, 20, font);
+
 }
 
 //--------------------------------------------------------------
@@ -47,19 +75,68 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    ofBackground(255*.15);
-    ofDisableAntiAliasing();
+    ofBackground(0);
     ofSetColor(255);
+    
+    
+    /*
     ofRect(monoLineTextInput.bounds);
     ofRect(multilineTextInput.bounds);
-    ofNoFill();
+    
     monoLineTextInput.draw();
     multilineTextInput.draw();
+    ofPushStyle();
+    // Draw the parameter display
+    parameterDisplay->draw();
+    ofPopStyle();
+     */
+    
+    // NF
+    // Access and modify NFValues in the order they were added
+    const std::vector<NFValue*>& drawOrder = nfNode.getDrawOrder();
+    size_t indexCounter=0;
+    for (const auto& nfValue : drawOrder) {
+        if (typeid(StringNFValue) == typeid(*nfValue)) {
+            StringNFValue* strNFValue = dynamic_cast<StringNFValue*>(nfValue);
+            ofDrawBitmapString(strNFValue->value.getName() + ": " + strNFValue->value.get(), 20, 220);
+            /*
+            ofRect(textInputFields[indexCounter].bounds);
+            ofNoFill();
+            textInputFields[indexCounter].draw();
+             */
+            
+        } else if (typeid(DoubleNFValue) == typeid(*nfValue)) {
+            DoubleNFValue* doubleNFValue = dynamic_cast<DoubleNFValue*>(nfValue);
+            ofDrawBitmapString(doubleNFValue->value.getName() + ": " + ofToString(doubleNFValue->value.get()), 20, 240);
+            /*
+            ofRect(textInputFields[indexCounter].bounds);
+            ofNoFill();
+            textInputFields[indexCounter].draw();
+             */
+        } else if (typeid(BoolNFValue) == typeid(*nfValue)) {
+            BoolNFValue* boolNFValue = dynamic_cast<BoolNFValue*>(nfValue);
+            ofDrawBitmapString(boolNFValue->value.getName() + ": " + ofToString(boolNFValue->value.get()), 20, 260);
+            /*
+            ofRect(textInputFields[indexCounter].bounds);
+            ofNoFill();
+            textInputFields[indexCounter].draw();
+             */
+        } else if (typeid(IntNFValue) == typeid(*nfValue)) {
+            IntNFValue* intNFValue = dynamic_cast<IntNFValue*>(nfValue);
+            ofDrawBitmapString(intNFValue->value.getName() + ": " + ofToString(intNFValue->value.get()), 20, 280);
+            /*
+            ofRect(textInputFields[indexCounter].bounds);
+            ofNoFill();
+            textInputFields[indexCounter].draw();
+             */
+        }
+        indexCounter++;
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::exit(){
-
+    
 }
 
 //--------------------------------------------------------------
