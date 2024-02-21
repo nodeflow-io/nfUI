b4a76f03d7c08787e56da922ac7595da023a4850
Matthias Strohmaier <ms@hostbuster.at>
2024-02-21 23:21:42 +0100

    refactoring

--- src/UI Components/NFValue.hpp
+++ src/UI Components/NFValue.hpp
@@ -15,31 +15,55 @@ namespace nfUI {
 class NFValue {
 public:
     virtual ~NFValue() {}
+
+    // Virtual function to return value as string
+    virtual std::string toString() const = 0;
 };
 
 // Example NFValues using ofParameter types
 class StringNFValue : public NFValue {
 public:
     ofParameter<std::string> value;
+
     StringNFValue(const std::string& initialValue) : value(initialValue) {}
+
+    std::string toString() const override {
+        return value.get();
+    }
 };
+
 
 class DoubleNFValue : public NFValue {
 public:
     ofParameter<double> value;
+
     DoubleNFValue(double initialValue) : value(initialValue) {}
+
+    std::string toString() const override {
+        return std::to_string(value.get());
+    }
 };
 
 class BoolNFValue : public NFValue {
 public:
     ofParameter<bool> value;
+
     BoolNFValue(bool initialValue) : value(initialValue) {}
+
+    std::string toString() const override {
+        return value.get() ? "true" : "false";
+    }
 };
 
 class IntNFValue : public NFValue {
 public:
     ofParameter<int> value;
+
     IntNFValue(int initialValue) : value(initialValue) {}
+
+    std::string toString() const override {
+        return std::to_string(value.get());
+    }
 };
 
 }
