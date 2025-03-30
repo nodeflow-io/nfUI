8b82556a1e1f195406fc398d0a096a2845acdecc
Matthias Strohmaier <ms@hostbuster.at>
2025-03-30 21:26:57 +0200

    refactoring wip

--- src/UI-Components/NFValue.cpp
+++ src/UI-Components/NFValue.cpp
@@ -6,3 +6,67 @@
 //
 
 #include "NFValue.hpp"
+
+namespace nfUI {
+
+// StringNFValue implementations
+StringNFValue::StringNFValue(const std::string& initialValue) : value(initialValue) {}
+
+std::string StringNFValue::toString() const {
+    return value.get();
+}
+
+nfAPI::ValueType StringNFValue::getType() const {
+    return nfAPI::ValueType::String;
+}
+
+bool StringNFValue::toBool() const {
+    return !value.get().empty();
+}
+
+// DoubleNFValue implementations
+DoubleNFValue::DoubleNFValue(double initialValue) : value(initialValue) {}
+
+std::string DoubleNFValue::toString() const {
+    return std::to_string(value.get());
+}
+
+nfAPI::ValueType DoubleNFValue::getType() const {
+    return nfAPI::ValueType::Double;
+}
+
+bool DoubleNFValue::toBool() const {
+    return value.get() != 0.0;
+}
+
+// BoolNFValue implementations
+BoolNFValue::BoolNFValue(bool initialValue) : value(initialValue) {}
+
+std::string BoolNFValue::toString() const {
+    return value.get() ? "1" : "0";
+}
+
+nfAPI::ValueType BoolNFValue::getType() const {
+    return nfAPI::ValueType::Bool;
+}
+
+bool BoolNFValue::toBool() const {
+    return value.get();
+}
+
+// IntNFValue implementations
+IntNFValue::IntNFValue(int initialValue) : value(initialValue) {}
+
+std::string IntNFValue::toString() const {
+    return std::to_string(value.get());
+}
+
+nfAPI::ValueType IntNFValue::getType() const {
+    return nfAPI::ValueType::Int;
+}
+
+bool IntNFValue::toBool() const {
+    return value.get() != 0;
+}
+
+} // namespace nfUI
--- src/UI-Components/NFValue.hpp
+++ src/UI-Components/NFValue.hpp
@@ -15,8 +15,7 @@ namespace nfUI {
 
 class NFValue {
 public:
-    virtual ~NFValue() {}
-    
+    virtual ~NFValue() = default;
     virtual std::string toString() const = 0;
     virtual bool toBool() const = 0;
     virtual nfAPI::ValueType getType() const = 0;
@@ -26,77 +25,42 @@ class StringNFValue : public NFValue {
 public:
     ofParameter<std::string> value;
     
-    StringNFValue(const std::string& initialValue) : value(initialValue) {}
-    
-    std::string toString() const override {
-        return value.get();
-    }
-    
-    nfAPI::ValueType getType() const override {
-        return nfAPI::ValueType::String;
-    }
-    
-    bool toBool() const override {
-        return !value.get().empty();
-    }
+    explicit StringNFValue(const std::string& initialValue);
+    std::string toString() const override;
+    nfAPI::ValueType getType() const override;
+    bool toBool() const override;
 };
 
 class DoubleNFValue : public NFValue {
 public:
     ofParameter<double> value;
     
-    DoubleNFValue(double initialValue) : value(initialValue) {}
-    
-    std::string toString() const override {
-        return std::to_string(value.get());
-    }
-    
-    nfAPI::ValueType getType() const override {
-        return nfAPI::ValueType::Double;
-    }
-    
-    bool toBool() const override {
-        return value.get() != 0.0;
-    }
+    explicit DoubleNFValue(double initialValue);
+    std::string toString() const override;
+    nfAPI::ValueType getType() const override;
+    bool toBool() const override;
 };
 
 class BoolNFValue : public NFValue {
 public:
     ofParameter<bool> value;
     
-    BoolNFValue(bool initialValue) : value(initialValue) {}
-    
-    std::string toString() const override {
-        return value.get() ? "1" : "0";
-    }
-    
-    nfAPI::ValueType getType() const override {
-        return nfAPI::ValueType::Bool;
-    }
-    
-    bool toBool() const override {
-        return value.get();
-    }
+    explicit BoolNFValue(bool initialValue);
+    std::string toString() const override;
+    nfAPI::ValueType getType() const override;
+    bool toBool() const override;
 };
 
 class IntNFValue : public NFValue {
 public:
     ofParameter<int> value;
     
-    IntNFValue(int initialValue) : value(initialValue) {}
-    
-    std::string toString() const override {
-        return std::to_string(value.get());
-    }
-    
-    nfAPI::ValueType getType() const override {
-        return nfAPI::ValueType::Int;
-    }
-    
-    bool toBool() const override {
-        return value.get() != 0;
-    }
+    explicit IntNFValue(int initialValue);
+    std::string toString() const override;
+    nfAPI::ValueType getType() const override;
+    bool toBool() const override;
 };
 
-}
+} // namespace nfUI
+
 #endif /* NFValue_hpp */
