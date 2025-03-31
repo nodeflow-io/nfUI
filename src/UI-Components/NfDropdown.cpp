c49d663969eb12239190b65cc45eb5f9e46043de
Matthias Strohmaier <ms@hostbuster.at>
2025-03-31 10:09:13 +0200

    refactoring wip

--- src/UI-Components/NfDropdown.cpp
+++ src/UI-Components/NfDropdown.cpp
@@ -6,3 +6,24 @@
 //
 
 #include "NfDropdown.hpp"
+
+namespace nfUI {
+
+NfDropdown::NfDropdown(const NfUIConfig& config, const std::string& name)
+    : NfBoxxer(config, name, std::make_unique<StringNFValue>(""))
+{
+}
+
+void NfDropdown::selectItem(int itemIndex) {
+    // Logic to handle item selection
+    nfUI::NfEventManager::getEventManager().emit("dropdown_selection", std::to_string(itemIndex));
+    // Close the modal after selection
+    // g_nodeManager.closeModalNode(_name);
+}
+
+void NfDropdown::draw() {
+    // Your drawing logic here
+    std::cout << "Drawing dropdown: " << _name << std::endl;
+}
+
+} // namespace nfUI
--- src/UI-Components/NfDropdown.hpp
+++ src/UI-Components/NfDropdown.hpp
@@ -16,24 +16,11 @@ namespace nfUI {
 
 class NfDropdown : public NfBoxxer {
 public:
-    NfDropdown(const NfUIConfig& config, const std::string& name)
-        : NfBoxxer(config, name, std::make_unique<StringNFValue>("")) {}
-
-    void selectItem(int itemIndex) {
-        
-        // Logic to handle item selection
-        nfUI::NfEventManager::getEventManager().emit("dropdown_selection", std::to_string(itemIndex));
-        // Close the modal after selection
-       // g_nodeManager.closeModalNode(_name);
-    }
-
-    // Override the draw method to render dropdown options
-    void draw() override {
-        // Your drawing logic here
-        std::cout << "Drawing dropdown: " << _name << std::endl;
-    }
+    NfDropdown(const NfUIConfig& config, const std::string& name);
+    void selectItem(int itemIndex);
+    void draw() override;
 };
 
-}
+} // namespace nfUI
 
 #endif /* NfDropdown_hpp */
